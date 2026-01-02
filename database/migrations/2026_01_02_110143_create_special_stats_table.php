2485ced454f5efa33b4e15f6dc735e2f9125059a
BoheN <87343009+Bohenek@users.noreply.github.com>
2026-01-02 13:46:02 +0100

    Modify special_stats table migration
    Add a nullable description field to special_stats table

--- database/migrations/2026_01_02_110143_create_special_stats_table.php
+++ database/migrations/2026_01_02_110143_create_special_stats_table.php
@@ -6,22 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('special_stats', function (Blueprint $table) {
             $table->id();
-            $table->string('name'); // Strength, Perception etc.
+            $table->string('name');
             $table->integer('value');
+            $table->text('description')->nullable(); 
+            
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('special_stats');
